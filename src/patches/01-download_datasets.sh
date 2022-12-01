47c43cae3ccb20f18013e021529f9128655b305e
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-12-01 16:57:17 +0100

    create data directory if it does not exist

--- src/patches/01-download_datasets.sh
+++ src/patches/01-download_datasets.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+mkdir -p computed
+
 wget \
     https://raw.githubusercontent.com/GXimingLu/a_star_neurologic/main/dataset/question_generation/constraints.jsonl \
     -O computed/qg_constraints.jsonl
